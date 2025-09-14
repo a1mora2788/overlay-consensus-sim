d82e787ce58b9418d98a9516a875ed70ddb8b1d4
a1mora2788 <A1mora2788@gmail.com>
2025-09-13 19:33:28 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-{
-  "buildCommand": "npm run build",
-  "outputDirectory": "dist",
-  "framework": "vite",
-  "routes": [
-    { "src": "/(.*)", "dest": "/" }
-  ]
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
